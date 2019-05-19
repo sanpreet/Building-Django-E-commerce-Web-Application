75e6bedc6a59d9199e30d968794fbbdeb32a3476
Sanpreet Singh <sany26.com@gmail.com>
2019-05-20 02:20:29 +0530

    initial commit
    This is the code written in python using Django framework. In short building small e-commerce website using Django.

---
+++ first_version/e_store_build_project/store_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from store_app import views
+
+
+urlpatterns = [
+    path('', views.catalog, name="catalog"),
+    path('cart/', views.cart, name="cart"),
+    path('cart/remove/', views.removefromcart, name="remove"),
+    path('cart/checkout/', views.checkout, name="checkout"),
+    path('cart/checkout/complete/', views.completeorder, name="completeorder"),
+]

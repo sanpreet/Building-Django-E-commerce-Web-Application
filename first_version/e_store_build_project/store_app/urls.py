5a8d0d0d18d074afc32d5312b00334cf6fe96957
Sanpreet Singh <sany26.com@gmail.com>
2019-05-27 23:58:06 +0530

    urls for admin panel and admin dashboard is added
    From the admin panel, I have redirected the authenticated user to the admin dashboard.

--- first_version/e_store_build_project/store_app/urls.py
+++ first_version/e_store_build_project/store_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('cart/remove/', views.removefromcart, name="remove"),
     path('cart/checkout/', views.checkout, name="checkout"),
     path('cart/checkout/complete/', views.completeorder, name="completeorder"),
+    path('admin-login/', views.adminLogin, name="admin_login"),
+    path('admin-panel/', views.adminDashboard, name="admin") 
 ]
